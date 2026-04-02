1a73b675551c39a408a546bab86604f591a7dd14
Geoff-Robin <jeffrobin132004@gmail.com>
2026-04-02 16:58:32 +0000

    shfiting everything to mapped columns and adding finnancial records

--- models.py
+++ models.py
@@ -1,23 +1,45 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, Enum, ForeignKey, Numeric, Date, Boolean, DateTime, func
 import enum
+import datetime
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 class UserRole(str, enum.Enum):
     viewer = "viewer"
     analyst = "analyst"
     admin = "admin"
 
+class FinancialType(str, enum.Enum):
+    income = "income"
+    expense = "expense"
+
 class User(Base):
     __tablename__ = "users"
 
-    id = mapped_column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-
-    role = Column(
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    role: Mapped[UserRole] = mapped_column(
         Enum(UserRole, name="user_roles"),
         default=UserRole.viewer,
         nullable=False
     )
+
+    records: Mapped[list["FinancialRecord"]] = relationship(back_populates="user")
+
+class FinancialRecord(Base):
+    __tablename__ = "financial_records"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    amount: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
+    type: Mapped[FinancialType] = mapped_column(Enum(FinancialType, name="financial_types"), nullable=False)
+    category: Mapped[str] = mapped_column(String, nullable=False)
+    date: Mapped[datetime.date] = mapped_column(Date, nullable=False)
+    notes: Mapped[str | None] = mapped_column(String, nullable=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+
+    user: Mapped["User"] = relationship(back_populates="records")
